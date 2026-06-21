cf96341b3d6f0fd623fe3e2e68a805beca456108
Katargina <katrin-kate@ukr.net>
2026-06-21 22:24:56 +0200

    done

---
+++ src/controllers/notesController.js
@@ -0,0 +1,66 @@
+import { Note} from '../models/note.js';
+import createHttpError from 'http-errors';
+
+// src/controllers/notesController.js
+
+// Отримати список усіх нотаток
+export const getAllNotes = async (req, res) => {
+  const notes = await Note.find();
+  res.status(200).json(notes);
+};
+
+// Отримати одну нотатку за id
+export const getNoteById = async (req, res) => {
+  const { noteId } = req.params;
+  const note = await Note.findById(noteId);
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(note);
+};
+
+// Створити нову нотатку
+
+export const createNote = async (req, res) => {
+  try {
+    const newNote = await Note.create(req.body);
+
+    res.status(201).json(newNote);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Видалити нотатку за id
+export const deleteNote = async (req, res) => {
+  const { noteId } = req.params;
+  const note = await Note.findOneAndDelete({
+    _id: noteId,
+  });
+
+  if (!note) {
+    throw createHttpError(404, "Note not found");
+  }
+
+  res.status(200).json(note);
+};
+
+// Оновлення нотатки за id
+export const updateNote = async (req, res) => {
+  const { noteId } = req.params;
+
+  const note = await Note.findOneAndUpdate(
+    { _id: noteId }, // Шукаємо по id
+    req.body,
+    { returnDocument: "after" }, // повертаємо оновлений документ
+  );
+
+  if (!note) {
+	throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(note);
+};
+
